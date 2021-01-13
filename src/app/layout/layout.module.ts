d137a30c0a702a88db523555be9e30bd1aac5b23
Maurice-K <maurice.i.kelly@gmail.com>
2021-01-12 20:30:02 -0600

    Added router module to layout module for header component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
+import {RouterModule} from "@angular/router";
 
 
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderComponent, FooterComponent]
 })
 export class LayoutModule { }
